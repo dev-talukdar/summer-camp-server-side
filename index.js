100ab7d9278e58ddd067715c54b786ee96f71b34
dev.talukdar <khejurbd@gmail.com>
2023-06-11 17:07:46 +0600

    delete operations created

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ij7qzua.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,7 +37,7 @@ async function run() {
         // carts collection api started here  
         app.get('/carts', async(req, res) => {
             const email = req.query.email;
-            console.log(email)
+            // console.log(email)
             if(!email){
                 res.send([])
             }
@@ -52,6 +52,13 @@ async function run() {
             console.log(pclass);
             const result = await cartCollection.insertOne(pclass)
             res.send(result);
+        })
+
+        app.delete('/carts/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await cartCollection.deleteOne(query);
+            res.send(result)
         })
 
         // Send a ping to confirm a successful connection
