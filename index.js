957facca170edc8d6df63ed898ce97fb920393c4
dev.talukdar <khejurbd@gmail.com>
2023-06-11 23:08:33 +0600

    instructor api is created

--- index.js
+++ index.js
@@ -57,7 +57,18 @@ async function run() {
                     role: 'admin'
                 },
             }
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
 
+        app.patch('/users/instructor/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
+                },
+            }
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.send(result)
         })
