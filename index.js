977a467a9f9183e32acbe9e945ea046ad4e24abf
dev.talukdar <khejurbd@gmail.com>
2023-06-12 06:44:06 +0600

    jwt verification middleware written

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+
+    // bearer token 
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ij7qzua.mongodb.net/?retryWrites=true&w=majority`;
@@ -35,31 +53,32 @@ async function run() {
         // jwt token generate
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, env.process.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
         })
 
 
         // user releted apis 
-        app.get('/users', async(req, res) => {
+        app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray()
             res.send(result)
         });
 
-        app.post('/users', async(req, res) => {
-            const user = req.body;            
-            const query = {email: user.email}
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
             const existingUser = await usersCollection.findOne(query)
-             
-            if(existingUser){
-                return res.send({message: 'user already exists'})
+
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
             }
             const result = await usersCollection.insertOne(user);
             res.send(result)
         });
 
-        app.patch('/users/admin/:id', async(req, res) => {
+        app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const updateDoc = {
                 $set: {
                     role: 'admin'
@@ -69,9 +88,9 @@ async function run() {
             res.send(result)
         })
 
-        app.patch('/users/instructor/:id', async(req, res) => {
+        app.patch('/users/instructor/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const updateDoc = {
                 $set: {
                     role: 'instructor'
@@ -89,13 +108,13 @@ async function run() {
         })
 
         // carts collection api started here  
-        app.get('/carts', async(req, res) => {
+        app.get('/carts', async (req, res) => {
             const email = req.query.email;
             // console.log(email)
-            if(!email){
+            if (!email) {
                 res.send([])
             }
-            const query = {email: email};
+            const query = { email: email };
             const result = await cartCollection.find(query).toArray();
             res.send(result);
         });
@@ -108,9 +127,9 @@ async function run() {
             res.send(result);
         })
 
-        app.delete('/carts/:id', async(req, res) => {
+        app.delete('/carts/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await cartCollection.deleteOne(query);
             res.send(result)
         })
